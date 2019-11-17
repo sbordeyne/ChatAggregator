889ece0ae5bf6f2645bd67c3773cb8e6a5da4832
Dogeek <Dogeek@users.noreply.github.com>
2019-11-17 22:01:21 +0100

    irc framework

---
+++ twitch_irc/irc.py
@@ -0,0 +1,150 @@
+import threading
+import socket
+import collections
+import time
+
+from .logging import logger
+
+
+lock = threading.RLock()
+
+
+class IRC(threading.Thread):
+    def __init__(self, server, nick, password, channel_name,
+                 port=6667, reqs=None, max_messages=100, message_callbacks=None,
+                 on_leave_callbacks=None, **kwargs):
+        """
+        IRC handler class, handling any IRC server, sending the necessary commands to join a
+        channel automatically, setting the name and pass.
+
+        :param server: server host name to join
+        :param nick: nickname of the bot
+        :param password: password or OAuth token to log in
+        :param channel_name: channel name to join
+        :param port: port for the IRC server (default : 6667)
+        :param reqs: permission request list. Sends CAP REQ commands to the IRC server. (default : [])
+        :param max_messages: maximum messages to send the server in 30 seconds. set to -1 for unlimited. (default:100)
+        :param message_callbacks: dictionary of callable/list of callables to map to specific IRC commands received.
+                                  callbacks will be passed a 'text' argument, which is the un-parsed text
+                                  the bot received. (default: defaultdict(list))
+        :param on_leave_callbacks: list of callables to call when the bot successfully
+                                   parts the IRC server. (default: [])
+        :param kwargs: kwargs to pass the threading.Thread superclass.
+        """
+        super().__init__(**kwargs)
+        self.running = True
+        self.message_queue = collections.deque()
+        self.number_of_messages_sent = 0
+        self.time_start_daemon = time.time()
+
+        self.server = server
+        self.nickname = nick
+        self.password = password
+        self.channel_name = channel_name
+        self.port = port
+        self.reqs = [] if reqs is None else reqs
+        self.max_messages = max_messages
+
+        if on_leave_callbacks is None:
+            self.on_leave_callbacks = []
+        elif callable(on_leave_callbacks):
+            self.on_leave_callbacks = [on_leave_callbacks]
+        elif isinstance(on_leave_callbacks, (list, tuple)):
+            self.on_leave_callbacks = list(on_leave_callbacks)
+        else:
+            raise ValueError("on_leave_callbacks kwargs must be None, a callable, a list or a tuple")
+
+        if message_callbacks is None:
+            self.message_callbacks = collections.defaultdict(list)
+        elif isinstance(message_callbacks, dict):
+            for k in message_callbacks:
+                if callable(message_callbacks[k]):
+                    message_callbacks[k] = [message_callbacks[k]]
+            self.message_callbacks = message_callbacks
+        else:
+            raise ValueError("message_callbacks kwargs must be None or a dictionary")
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.server, self.port))
+        self.socket.setblocking(False)
+
+        self.send(f"PASS {self.password}")
+        self.send(f"NICK {self.nickname}")
+        self.send(f"JOIN {self.channel_name}")
+        for req in self.reqs:
+            self.send(f"CAP REQ:{req}")
+
+    def send(self, text):
+        """
+        Sends a command to the IRC server. Appends a newline if the text doesn't have one already.
+
+        Silently fails if the amount of messages exceeds the limit.
+        :param text: command to send the server.
+        :return: Boolean. True if the command was sent successfully. False otherwise (the limit has been exceeded)
+        """
+        if self.number_of_messages_sent < self.max_messages or self.max_messages < 0:
+            self.number_of_messages_sent += 1
+            if not text.endswith("\n"):
+                text += '\n'
+            self.socket.send(text.encode("utf8"))
+            return True
+        else:
+            return False
+
+    def send_message(self, message):
+        """
+        Sends a text message using the PRIVMSG command.
+
+        :param message: message to send to the server.
+        :return: None
+        """
+        self.send(f"PRIVMSG #{self.channel_name} : {str(message)}")
+
+    @property
+    def last_message(self):
+        """
+        Pops the first text message from the queue.
+
+        :return: str, first message of the queue.
+        """
+        return self.message_queue.popleft()
+
+    def run(self):
+        """
+        Runs the thread and process messages.
+
+        :return: None
+        """
+        while self.running:
+            with lock:
+                if int(time.time() - self.time_start_daemon) % 30 == 0:
+                    self.number_of_messages_sent = 0
+                try:
+                    texts = self.socket.recv(4096).decode().split('\n')
+                    for text in texts:
+                        if text.startswith('PING'):  # Prevent time out
+                            self.send(f'PONG {text.split()[1]}\r')
+                        elif text.startswith("PRIVMSG"):
+                            self.message_queue.append(text)
+
+                        for k in self.message_callbacks:
+                            if text.startswith(k.upper()):
+                                for cb in self.message_callbacks.get(k, []):
+                                    cb(text)
+                except socket.error as e:
+                    if "10035" not in repr(e):
+                        logger.exception("{} : {}".format(type(e), e))
+                    continue
+
+    def quit(self):
+        """
+        Quits the thread and stops the socket after parting the bot.
+
+        :return: None
+        """
+        if self.socket.send('PART'.encode()):
+            self.socket.close()
+            self.running = False
+            for cb in self.on_leave_callbacks:
+                cb()
+
---
+++ twitch_irc/logging.py
@@ -0,0 +1,3 @@
+import logging
+
+logger = logging.Logger("twitch-irc.log")
